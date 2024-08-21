e782f7b0c7af4a457b451ed816993514635d6e87
Amol Anand <amol.anand@gmail.com>
2024-08-21 12:03:45 -0400

    chore: lint fixes

--- tools/admin-edit/admin-edit.js
+++ tools/admin-edit/admin-edit.js
@@ -6,29 +6,30 @@ const method = document.getElementById('method');
 const console = document.getElementById('console');
 
 function consoleLog(message) {
-    console.value += `${message}\n`;
-    console.scrollTop = console.scrollHeight;
+  console.value += `${message}\n`;
+  console.scrollTop = console.scrollHeight;
 }
 
 fetchButton.addEventListener('click', async () => {
-    localStorage.setItem('admin-url', adminURL.value);
-    const resp = await fetch(adminURL.value);
-    const text = await resp.text();
-    textarea.value = text;
-    consoleLog(`${resp.status} GET ${adminURL.value} ${resp.headers.get('x-error') || ''}`);
+  localStorage.setItem('admin-url', adminURL.value);
+  const resp = await fetch(adminURL.value);
+  const text = await resp.text();
+  textarea.value = text;
+  consoleLog(`${resp.status} GET ${adminURL.value} ${resp.headers.get('x-error') || ''}`);
 });
 
 saveButton.addEventListener('click', async () => {
-    localStorage.setItem('admin-url', adminURL.value);
-    const resp = await fetch(adminURL.value, {
-        method: method.value,
-        body: textarea.value,
-        headers: {
-            'content-type': adminURL.value.endsWith('.yaml') ? 'text/yaml' : 'application/json',
-        }
-    });
-    const text = await resp.text();
-    consoleLog(`${resp.status} ${method.value} ${adminURL.value} ${resp.headers.get('x-error') || ''}`);
+  localStorage.setItem('admin-url', adminURL.value);
+  const resp = await fetch(adminURL.value, {
+    method: method.value,
+    body: textarea.value,
+    headers: {
+      'content-type': adminURL.value.endsWith('.yaml') ? 'text/yaml' : 'application/json',
+    },
+  });
+  // eslint-disable-next-line no-unused-vars
+  const text = await resp.text();
+  consoleLog(`${resp.status} ${method.value} ${adminURL.value} ${resp.headers.get('x-error') || ''}`);
 });
 
-adminURL.value = localStorage.getItem('admin-url')||'https://admin.hlx.page/status/adobe/aem-boilerplate/main/';
+adminURL.value = localStorage.getItem('admin-url') || 'https://admin.hlx.page/status/adobe/aem-boilerplate/main/';
